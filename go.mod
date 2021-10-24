91d5fed9741da8f2e5471c90a488024fc20b7151
Arman Torebekov <torebek.arman14@gmail.com>
2021-10-24 23:16:34 +0600

    v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Torebekov/ex_less2
+
+go 1.17
+
+require github.com/Torebekov/lib_less2 v1.0.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Torebekov/lib_less2"
+)
+
+func main() {
+	fmt.Println(lib_less2.ChangeCase("oNElAB"))
+}
