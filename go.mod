70887322ed930a9c5959e1e978d5c45c3a7a5773
Arman Torebekov <torebek.arman14@gmail.com>
2021-10-24 23:35:14 +0600

    v1.0.1

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Torebekov/ex_less2
 
 go 1.17
 
-require github.com/Torebekov/lib_less2 v1.0.0
+require github.com/Torebekov/lib_less2 v1.0.1
--- main.go
+++ main.go
@@ -6,5 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(lib_less2.ChangeCase("oNElAB"))
+	fmt.Println(lib_less2.FindRoots(2,5,2))
+	fmt.Println(lib_less2.FindRoots(1,4,4))
+	fmt.Println(lib_less2.FindRoots(2,1,2))
 }
